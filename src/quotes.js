84b1c30efaf85b8401313206db14696d364242b5
Anil Seervi <61609033+AnilSeervi@users.noreply.github.com>
2022-01-08 14:45:43 +0530

    ✨ More quotes: Steal like an artist

--- src/quotes.js
+++ src/quotes.js
@@ -1,4 +1,4 @@
-// some good-for rules
+// some good-to-have rules
 // try to use right single quotation mark `’` (String.fromCharCode(0x2019)), not apostrophe `'` (String.fromCharCode(0x27))
 // put those strings in backticks || linter gon complain
 // source?: String , is optional but helpful.
@@ -40,12 +40,91 @@ const quotes = [
 	{
 		quote: `The simple thing I’ve learned over the years is just to have a starting point and once you have a starting point the work seems to make itself.`,
 		author: `David Shrigley`,
-		source: `Keep Going - Austin Kleon`,
 	},
 	{
 		quote: `There’s never a space under paintings in a gallery where someone writes their opinion.`,
 		author: `Natalie Dee`,
-		source: `Show Your Work! - Austin Kleon`,
+	},
+	{
+		quote: `If you don’t produce, you won’t thrive - no matter how skilled or talented you are.`,
+		author: `Cal Newport`,
+		source: `Deep Work`,
+	},
+	{
+		quote: `The only art I’ll ever study is stuff that I can steal from.`,
+		author: `David Bowie`,
+	},
+	{
+		quote: `Everything that needs to be said has already been said. But, since no one was listening, everyting must be said again.`,
+		author: `André Gide`,
+	},
+	{
+		quote: `What is originality? Undetected plagiarism.`,
+		author: `William Ralph Inge`,
+	},
+	{
+		quote: `It is better to take what does not belong to you than to let it lie around neglected.`,
+		author: `Mark Twain`,
+	},
+	{
+		quote: `Fake it ’till you make it.`,
+		author: `Unknown`,
+	},
+	{
+		quote: `Those who so not want to imitate anything, produce nothing.`,
+		author: `Salvador Dalí`,
+	},
+	{
+		quote: `If you have one person you’re influenced by, everyone will say you’re the next whoever. But if you rip off a hundred people, everyone will say you’re so original!`,
+		author: `Gary Panter`,
+	},
+	{
+		quote: `I have stolen all these moves from all these great players. I just try to do them proud, the guys who came before, because I learned so much from them. It’s all in the name of game. It’s a lot bigger than me.`,
+		author: `Kobe Bryant`,
+	},
+	{
+		quote: `We don’t know where we get our ideas from. What we do know it that we do not get them from our laptops.`,
+		author: `John Cleese`,
+	},
+	{
+		quote: `You never really get to touch anything that you’re doing unless you print it out.`,
+		author: `Stanley Donwood`,
+	},
+	{
+		quote: `The work you do while you procrastinate is probably the work you should be doing for the rest of your life.`,
+		author: `Jessica Hische`,
+	},
+	{
+		quote: `Avoiding work is the way to focus my mind.`,
+		author: `Maira Kalman`,
+	},
+	{
+		quote: `You can’t connect the dots looking forward, you can only connect them looking backwards.`,
+		author: `Steve Jobs`,
+	},
+	{
+		quote: `Don’t worry about people stealing your ideas. If your ideas are any good, you’ll have to ram them down people’s throats.`,
+		author: `Howard Aiken`,
+	},
+	{
+		quote: `The only mofos in my circle are people that I can learn from.`,
+		author: `Ouestlove`,
+	},
+	{
+		quote: `Garbage in, Garbage out.`,
+		author: `Unknown`,
+	},
+	{
+		quote: `The best way to get approval is to not need it.`,
+		author: `Hugh MacLeod`,
+	},
+	{
+		quote: `Be regular and orderly in your life, so that you may be violent and original in your work.`,
+		author: `Gustave Flaubert`,
+	},
+	{
+		quote: `If you don’t take money, they can’t tell you what to do.`,
+		author: `Bill Cunningham`,
 	},
 ]
 
