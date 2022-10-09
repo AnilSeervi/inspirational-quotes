3ccd2790993b21aba247f067c3159cbfed561bf6
Sat Naing <satnaingdev@gmail.com>
2022-10-10 00:29:20 +0630

    :white_check_mark: Add test to check for duplicate quotes (#21)
    * :white_check_mark: Add test to check for duplicate quotes
    
    * :recycle: Extract allQuotes and put it inside beforeEach

--- tests/basic.test.ts
+++ tests/basic.test.ts
@@ -1,10 +1,15 @@
-import { assert, describe, expect, test } from 'vitest'
+import { assert, beforeEach, describe, expect, test } from 'vitest'
 import { getAll, getRandom } from '../src/index'
 import { quotes } from '../src/quotes'
+import { Quote } from '../src/types'
 
 describe(`All Quotes`, () => {
+	let allQuotes: Quote[]
+	beforeEach(() => {
+		allQuotes = getAll()
+	})
+
 	test(`Test 01: Get all Quotes`, () => {
-		const allQuotes = getAll()
 		expect(allQuotes).toEqual(quotes)
 		assert(allQuotes.length > 0)
 		for (const quote of allQuotes) {
@@ -14,17 +19,21 @@ describe(`All Quotes`, () => {
 	})
 
 	test(`Test 02: All Quotes end with a period`, () => {
-		const allQuotes = getAll()
 		for (const quote of allQuotes) {
 			assert(quote.quote.endsWith(`.`) || quote.quote.endsWith(`!`))
 		}
 	})
 
 	test(`Test 03 : Check for proper quote format`, () => {
-		const allQuotes = getAll()
 		for (const quote of allQuotes) {
 			assert(!quote.quote.includes(`'`))
 		}
+	})
+
+	test(`Test 04 : Check for duplicate quotes`, () => {
+		const allQuoteStrings = allQuotes.map((quoteObj) => quoteObj.quote)
+		const uniqueQuoteStrings = [...new Set(allQuoteStrings)]
+		expect(allQuoteStrings).toEqual(uniqueQuoteStrings)
 	})
 })
 
